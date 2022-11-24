b3ff341c66981ccabc9f70d609ce97fcb11d8441
cootshk <cootshk1@gmail.com>
2022-11-23 22:35:32 -0600

    fixed the first real commit (wrong directory lolol)

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
-# Classifier
+![syberian husky](https://www.akc.org/wp-content/uploads/2017/11/Siberian-Husky-standing-outdoors-in-the-winter.jpg)
+# Dog Breed Classifier
 
 *Note: the [dusty-nv/jetson-inference](https://www.github.com/dusty-nv/jetson-inference) repository is included. If you already have this setup, skip the first few steps.*
 
-![add image descrition here](direct image link here)
+
 
 ## The Algorithm
 
-Add an explanation of the algorithm and how it works. Make sure to include details about how the code works, what it depends on, and any other relevant info. Add images or other descriptions for your project here. 
+Step One: arguements (python script): use flags if you need them. Flags:
+```
+-h True or --headed True: shows the camera window (warning: can be very laggy) (default is False)
+-t {number} or --threashold {number}: how confident you have to be that there's a dog to determine species (default is 0.9)
+-d {path/to/device} or --device {path/to/device}: the camera or RTP/RTSP stream to be used, accepted protocols are csi, file, v4l2, rtp, and rtsp (default is /dev/video0 (usb camera), use ls /dev/video* to check your connected usb devices)
+-f {path/to/file} or --filename {path/to/file}: the file/path to file to use for image/video classification (defaults to dog). **DO NOT INCLUDE THE FILE EXTENSION IN THIS ARGUEMENT**
+```
+Step Two: find dogs in the video: The first thing the program does is loads the video passed in the previous step, and checks for dogs using the COCO-Dogs model.
+
+Step Three: once a dog is found, classify the breed: When the program detects a dog with the probability over the threashold, it will save an image to the filename.jpg, then will classify that image using googlenet.
+
+Step Four: output and repeat: Outputs the specified breed, then repeats back to step 2 
+
+*for a more in-depth explanation, feel free to look at the comments in the python files*
 
 ## Running this project
 THIS PROGRAM REQUIRES A **JETSON NANO**
@@ -19,4 +33,4 @@ THIS PROGRAM REQUIRES A **JETSON NANO**
 7. Install the Google-net image classification models **You do NOT need to install Pytorch**
 8. install the jetson-inference library `make;sudo make install;sudo ldconfig`
 
-[View a video explanation here](video link)
+[Demonstration](video link)
---
+++ breeddetector.py
@@ -0,0 +1,76 @@
+import jetson_inference
+import jetson_utils
+from jetson_utils import cudaCrop,cudaDeviceSynchronize,cudaAllocMapped, saveImage
+import argparse
+import time
+import os
+
+parser = argparse.ArgumentParser(description="Find the breed of dog in an image",formatter_class=argparse.RawTextHelpFormatter,epilog="uh idk what to put here")
+parser.add_argument("--headed", "-h",type=bool,default=False,help="True/False, If the camera window should be opened. Defaults to False")
+parser.add_argument("--threashold","-t", type=float, default=.9,help="Percent needed for species identification. Use 0 to disable. Defaults to 0.9")
+parser.add_argument("--device","-d", type=str,default="/dev/video0",help="Camera/RTP Stream to use. Defaults to /dev/video0 (usb camera). Use the command ls /dev/video* to check devices")
+parser.add_argument("--filename","-f", type=str,default="dog",help="The filename to use for the image and videos (use a path to save outside of the current directory). DO NOT INCLUDE A FILE EXTENSION")
+args = parser.parse_known_args()[0]
+headless = bool(args.headed)
+if args.threashold > 1:
+    percentneeded = float(args.threashold)/100
+else:
+    percentneeded = float(args.threashold)
+filename = str(args.filename).replace(".mp4","").replace(".jpg","")
+
+#Instanciate Detection Objects
+#net = jetson_inference.detectNet("ssd-mobilenet-v2",threshold=0.5)
+starttime = time.time() #get the time before loading the objectnet
+video_net = jetson_inference.detectNet("coco-dog",['--log-level=error'],threshold=0.5) #load the objectnet
+print(f"Time spent to Load Video: {int(time.time()-starttime)} seconds") #find loading duration
+
+# Define the video Source.
+video_camera = jetson_utils.videoSource(args.device) #define the video camera with -d
+
+if headless:
+    print("program is running in headless mode")
+else:
+    display = jetson_utils.videoOutput(f"file://{args.filename}.mp4") #create file to stream to window
+
+#Instanciate Image Recognition Objects
+starttime = time.time() #get time before loading the imagenet
+image_net = jetson_inference.imageNet('googlenet',['--log-level=error']) #load the aforementioned imagenet
+print(f"Time spent to Create Network: {int(time.time()-starttime)} seconds") #get time after loading the aforementioned aforementioned imagenet, and subtract the aforementioned time before loading the aforementioned aforementioned aforementioned imagenet
+
+#its loop time
+dogsdetected = 0
+try:
+    while True:
+        img = video_camera.Capture() #capture frame
+        detections = video_net.Detect(img) #start detection
+        if headless:
+            pass
+        else:
+            display.Render(img) #create/update window
+            display.SetStatus("Object Detection | Network {:.0f} FPS".format(video_net.GetNetworkFPS())) #set title
+        ### Dog detection time (dog/cat/person/etc)
+        #Get ClassID for the objects
+        for detection in detections: #for every detection
+            class_idx = detection.ClassID #object types
+            class_confidence = detection.Confidence #confidence
+            class_desc = video_net.GetClassDesc(class_idx) #descriptions for every class (i.e. Dog), breed identification coming soon
+            #print(f"{class_desc} at {class_confidence*100}%") #uncomment this if you want to hear about everything detected (prints "dog at 90%")
+
+           ###Breed identification time (i.e. poodle, husky)
+            if class_desc =="dog" and class_confidence >= int(percentneeded): #if its confident that we found a dog
+                print(f"Dog found with {class_confidence * 100}% confidence") #ok we found a dog, time to classify it
+                roi = (int(detection.Left), int(detection.cTop), int(detection.Right), int(detection.Bottom)) #dimensions of boundary box
+                snapshot = cudaAllocMapped(width=roi[2]-roi[0], height=roi[3]-roi[1], format=img.format) #take photo
+                cudaCrop(img, snapshot, roi) #crop photo
+                cudaDeviceSynchronize()
+                saveImage(f"{args.filename}.jpg",snapshot) #save the image in the directory specified with -d
+                del snapshot
+                dog_img = jetson_utils.loadImage("dog.jpg") #gotta reload that image, man
+                dog_idx, confidence = image_net.Classify(dog_img) #classify dog selection
+                dog_class_desc = image_net.GetClassDesc(dog_idx) #breed class ids
+                print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(dog_class_desc, dog_idx, confidence * 100)) #print statement
+                dogsdetected+=1
+except Exception as e: #if code is stopped
+    if isinstance(e,KeyboardInterrupt): #if Ctrl+C is used
+        print("Stopping!")
+        print(f"You found {dogsdetected} dogs!") #cool dog count
